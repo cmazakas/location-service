2710e751c52216f4ba2cf0216a09dc51d823587c
LeonineKing1199 <christian.mazakas@gmail.com>
2017-04-02 20:50:10 -0700

    Finally got a successful CSV parser!

--- test/parsing_tests.cpp
+++ test/parsing_tests.cpp
@@ -77,6 +77,45 @@ struct key_value_list : qi::grammar<Iterator, space_type>
   }
 };
 
+template <typename Iterator>
+struct zip_code_csv_grammar : qi::grammar<Iterator, spirit::ascii::space_type>
+{
+  using rule_type = qi::rule<Iterator, spirit::ascii::space_type>;
+
+  rule_type start;
+
+  rule_type zip;
+  rule_type city;
+  rule_type state;
+  rule_type latitude;
+  rule_type longitude;
+  rule_type timezone;
+  rule_type dst;
+
+  zip_code_csv_grammar(void) : zip_code_csv_grammar::base_type{start}
+  {
+    char const quote_char = '"';
+
+    zip   = quote_char >> +qi::alnum >> quote_char;
+    city  = quote_char >> +(~qi::char_(quote_char)) >> quote_char;
+    state = quote_char >> +(~qi::char_(quote_char)) >> quote_char;
+
+    latitude  = quote_char >> qi::double_ >> quote_char;
+    longitude = quote_char >> qi::double_ >> quote_char;
+
+    timezone = quote_char >> qi::int_ >> quote_char;
+    dst      = quote_char >> qi::int_ >> quote_char;
+    
+    start = *(zip
+      >> ',' >> city 
+      >> ',' >> state 
+      >> ',' >> latitude 
+      >> ',' >> longitude
+      >> ',' >> timezone
+      >> ',' >> dst);    
+  }
+};
+
 TEST_CASE("Sample CSV Testing")
 {
   std::string input{
@@ -89,10 +128,25 @@ TEST_CASE("Sample CSV Testing")
   auto begin = input.begin();
   auto end   = input.end();
 
-  auto const valid_str = boost::spirit::qi::phrase_parse(
+  auto const is_valid_str = boost::spirit::qi::phrase_parse(
     begin, end, 
     key_value_list<decltype(begin)>{}, 
     boost::spirit::ascii::space_type{});
 
-  REQUIRE(valid_str == true);
+  REQUIRE(is_valid_str == true);
+}
+
+TEST_CASE("Zip code sample csv line")
+{
+  std::string const input{"\"00210\",\"Portsmouth\",\"NH\",\"43.005895\",\"-71.013202\",\"-5\",\"1\""};
+
+  auto begin = input.begin();
+  auto end   = input.end();
+
+  auto const is_valid_str = qi::phrase_parse(
+    begin, end,
+    zip_code_csv_grammar<decltype(begin)>{},
+    spirit::ascii::space_type{});
+
+  REQUIRE(is_valid_str);
 }
